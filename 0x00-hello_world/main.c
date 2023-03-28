6fef7caf1fe0d9f815dab61bfc1ca6c26c11f1dc
potatological <te.zahra@gmail.com>
2023-03-28 00:20:06 -0700

    add 100-intel

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  printf ("Holberton School")
-  return (0)
+  return (0);
 }
