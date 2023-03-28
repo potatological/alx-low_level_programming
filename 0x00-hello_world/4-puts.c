ca6fab98e5233ff508f0443f42a21440c6a44ced
potatological <te.zahra@gmail.com>
2023-03-27 23:10:24 -0700

    add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@ int main(void)
    * puts - prints the sentence below
    * return 0 - allows the exit status of the program
    */
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
