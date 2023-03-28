eeeccf6630a2b50f841c351fbb3aabd6330567ac
potatological <te.zahra@gmail.com>
2023-03-28 00:35:46 -0700

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 /**
  * main - prints to stderr
  *
+ *
+ *
  *Return - 1 (success)
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
