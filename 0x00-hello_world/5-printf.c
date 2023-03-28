6ccb5880fd39f1f9807d7c587b5f1c21f0c16a22
potatological <te.zahra@gmail.com>
2023-03-27 23:47:09 -0700

    add 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints the sentence given
- * 
+ * main - prints the sentence given 
  * Return - Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-  printf ("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
