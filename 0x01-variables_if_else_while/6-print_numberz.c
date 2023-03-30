4d47f7ca74f74ee9088274998c9fe7fb34249224
potatological <te.zahra@gmail.com>
2023-03-30 00:25:54 -0700

    add 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-  int a = 0;
-  while (a < 10)
-    {
-      putchar(a);
-      a++;
-    }
-  putchar ('\n');
-  return (0);
+int a = 0;
+while (a < 10)
+{
+putchar(48+a);
+a++;
+}
+putchar ('\n');
+return (0);
 }
