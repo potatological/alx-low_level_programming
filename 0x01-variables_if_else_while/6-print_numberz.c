dbbbfd8d881163ebe5d7ec60215330d986f39bf7
potatological <te.zahra@gmail.com>
2023-03-30 00:24:02 -0700

    add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *        prints the single digit numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  int a = 0;
+  while (a < 10)
+    {
+      putchar(a);
+      a++;
+    }
+  putchar ('\n');
+  return (0);
+}
