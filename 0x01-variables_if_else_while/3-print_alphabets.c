6a79fe162e4b167d4c51479b6851f6d74b1a5c0c
potatological <te.zahra@gmail.com>
2023-03-29 23:37:45 -0700

    add 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,18 @@
  */
 int main(void)
 {
-  char c = 'a';
-  char b = 'A';
-  while (c <= 'z')
-    {
-      putchar(c);
-      c++;
-    }
-  while (b <= 'Z')
-    {
-      putchar(b);
-      b++;
-    }
-  putchar('\n');
-  return (0);
+char c = 'a';
+char b = 'A';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+while (b <= 'Z')
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return (0);
 }
