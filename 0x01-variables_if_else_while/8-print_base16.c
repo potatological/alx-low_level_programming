98f3e7357dba233bc924a2720965af97faf06e4e
potatological <te.zahra@gmail.com>
2023-03-30 01:07:55 -0700

    add 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,18 +8,18 @@
  */
 int main(void)
 {
-  char c = 'a';
-  int a = 0;
-  while (a < 10)
-    {
-      putchar(48 + a);
-      a++;
-    }
-  while (c <= 'f')
-    {
-      putchar(c);
-      c++;
-    }
-  putchar('\n');
-  return (0);
+char c = 'a';
+int a = 0;
+while (a < 10)
+{
+putchar(48 + a);
+a++;
+}
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
