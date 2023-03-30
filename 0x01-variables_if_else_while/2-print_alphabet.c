579a3aab7b123fc293b822ff07751b6800d04471
potatological <te.zahra@gmail.com>
2023-03-29 23:21:13 -0700

    add 1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point 
+ *        program that prints lowercase alphabets
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+putchar('\n');
+c++;
+}
+return (0);
+}
