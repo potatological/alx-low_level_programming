af81933910a8990e5bfedb767dad172480924ea9
potatological <te.zahra@gmail.com>
2023-04-01 11:24:25 -0700

    update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,22 @@
  */
 int main(void)
 {
-  int i;
-  int j;
-  for (i=0; i < 9; i++)
-    {
-      for (j=i+1; j < 10; j++)
-	{
-	  if (i == j)
-	    continue;
-	  putchar(i+'0');
-	  putchar(j+'0');
-	  if (i == 8 && j == 9)
-	    continue;
-	  putchar(',');
-	  putchar(' ');
-	  putchar('\n');
-	}
-    }
-  return (0);
+int i;
+int j;
+for (i=0; i < 9; i++)
+{
+for (j=i+1; j < 10; j++)
+{
+if (i == j)
+continue;
+putchar(i+'0');
+putchar(j+'0');
+if (i == 8 && j == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
