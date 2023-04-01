c80d6d6e060ee467ab4249cef099ab3d2fd2b722
potatological <te.zahra@gmail.com>
2023-04-01 13:08:08 -0700

    add 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,17 @@ int main(void)
 {
 int i;
 int j;
-for (i = 0; i < 100; i++)
+for (i = 0; i < 99; i++)
 {
-for (j = 0; j < 100; j++)
+for (j = i + 1; j < 100; j++)
 {
 if (i == j)
 continue;
-putchar(i/10 + '0');
-putchar(i%10 + '0');
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-putchar(j/10 + '0');
-putchar(j%10 + '0');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
 if (i == 98 && j == 99)
 continue;
 putchar(',');
