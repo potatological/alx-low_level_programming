29afd336febd2ca747e2d811921478229b3c0a2b
potatological <te.zahra@gmail.com>
2023-04-02 16:08:23 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,16 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @a: the parameter that stores the number
- * @b: the parameter that stores the last digit of a number 
+ * a: the parameter that stores the number
+ * b: the parameter that stores the last digit of a number
  * Return: returns the last digit b
  */
 
 int print_last_digit(int a)
 {
 int b;
-b = a % 10;
+write (b = a % 10);
 if (b < 0)
-b = b * -1;
+write (b = b * -1);
 return (b);
 }
