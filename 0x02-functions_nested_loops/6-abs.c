cba8c0a51ba42e33c81c2979d1f042505d22b413
potatological <te.zahra@gmail.com>
2023-04-01 22:38:59 -0700

    add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @a: the integer to have the absolute value of
+ */
+
+int _abs(int)
+{
+int a;
+if (a >= 0)
+{
+a = a;
+printf("%d\n", a);
+}
+else
+{
+a = -a;
+printf("%d\n", a);
+}
+}
