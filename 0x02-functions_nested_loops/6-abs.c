bdff1891ccd066eea6fcaa3a91b3ac3b1c2fc10f
potatological <te.zahra@gmail.com>
2023-04-01 22:41:53 -0700

    add 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _abs - prints the absolute value of an integer
  * @a: the integer to have the absolute value of
+ * Return: 0.
  */
 
-int _abs(int)
+int _abs(int a)
 {
-int a;
 if (a >= 0)
 {
 a = a;
@@ -18,4 +19,5 @@ else
 a = -a;
 printf("%d\n", a);
 }
+return(0);
 }
