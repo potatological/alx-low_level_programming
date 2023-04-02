421a4b5bd8bfa524fd880414d4f3f73b13e9d0a8
potatological <te.zahra@gmail.com>
2023-04-01 21:48:34 -0700

    add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - the entry point
+ *                  function that allow us to print all alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+char a;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
