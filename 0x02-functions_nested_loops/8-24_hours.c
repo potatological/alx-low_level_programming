1fe637fc90bd27d8e6a5d471db50c0c92a4958f3
potatological <te.zahra@gmail.com>
2023-04-02 17:27:40 -0700

    update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,30 @@
 
 void jack_bauer(void)
 {
-  int i;
-  int j;
-  for (i = 00; i < 24; i++)
-    {
-      for (j = 00; j < 60; j++)
-	{
-	  _putchar(i + '0');
-	  _putchar(':');
-	  _putchar(j + '0');
-	  _putchar('\n');
-	}
-    }
+int i;
+int j;
+int k;
+int l;
+for (i = 0; i <= 2; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+for (k = 0; k <= 5; k++)
+{
+for (l = 0; l <= 9; l++)
+{
+if (i == 2 && j == 4)
+{
+continue;
+}
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
+}
+}
+}
+}
 }
