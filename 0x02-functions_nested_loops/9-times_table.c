df45ed9ad48cb682bae034eca5ffb9292cff4163
potatological <te.zahra@gmail.com>
2023-04-02 18:05:41 -0700

    add 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,11 +16,12 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 mult = i * j;
-_putchar(mult + '0');
+_putchar('mult');
 if (j < 9)
 {
 _putchar(' ');
 _putchar(',');
+_putchar('\n');
 }
 }
 }
