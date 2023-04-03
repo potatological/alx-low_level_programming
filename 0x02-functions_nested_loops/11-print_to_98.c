8beebd1e33f71e82ab30c7bec2984fcf1a33b220
potatological <te.zahra@gmail.com>
2023-04-02 19:38:05 -0700

    add 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (;n <= 98; n++)
+for (; n <= 98; n++)
 {
 if (n == 98)
 {
@@ -29,7 +29,7 @@ printf("%d, ", n);
 }
 else
 {
-for (; n >=98; n--)
+for (; n >= 98; n--)
 {
 if (n == 98)
 {
