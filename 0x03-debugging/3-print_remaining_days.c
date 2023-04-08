ddb909f6691106369d5788426a6862baf7aabedc
potatological <te.zahra@gmail.com>
2023-04-07 21:07:14 -0700

    update three 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,14 +14,10 @@ void print_remaining_days(int month, int day, int year)
 {
 if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
 {
-if (month >= 2 && day >= 60)
+if (month >= 3 && day >= 60)
 {
-if (month == 2 && day == 60)
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-}
 day++;
+}
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
@@ -35,7 +31,6 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
-}
 }
 }
 }
