c0d2a5450712da88a2f453ca0e0796d98c09cf67
potatological <te.zahra@gmail.com>
2023-04-07 20:10:34 -0700

    update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,6 +16,15 @@ if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
 {
 if (month >= 2 && day >= 60)
 {
+if (month == 2 && day == 60)
+{
+continue;
+}
+else
+{
+day++;
+}
+ 
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
@@ -29,7 +38,6 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
-}
 }
 }
 }
