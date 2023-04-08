4c0804eafad74dde9107b13fc1d9a320f0b7b3d5
potatological <te.zahra@gmail.com>
2023-04-08 11:09:16 -0700

    update two 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,41 +8,41 @@
 
 void print_times_table(int n)
 {
-int i, j, a;
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-a = (i * j);
-if (n > 15 || n < 0)
-continue;
-else if (j == 0)
-_putchar('0');
-else if (a <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(a + '0');
-}
-else if (a <= 99 && a >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((a / 10) + '0');
-_putchar((a % 10) + '0');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar((a / 100) + '0');
-_putchar(((a - 100) / 10) + '0');
-_putchar((a % 10) + '0');
-}
-}
-_putchar('\n');
-}
+  int i, j, a;
+  for (i = 0; i <= n; i++)
+    {
+      for (j = 0; j <= n; j++)
+	{
+	  a = (i * j);
+	  if (n > 15 || n < 0)
+	    return ();
+	  else if (j == 0)
+	    _putchar('0');
+	  else if (a <= 9)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar(' ');
+	      _putchar(' ');
+	      _putchar(a + '0');
+	    }
+	  else if (a <= 99 && a >= 10)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar(' ');
+	      _putchar((a / 10) + '0');
+	      _putchar((a % 10) + '0');
+	    }
+	  else
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar((a / 100) + '0');
+	      _putchar(((a - 100) / 10) + '0');
+	      _putchar((a % 10) + '0');
+	    }
+	}
+      _putchar('\n');
+    }
 }
