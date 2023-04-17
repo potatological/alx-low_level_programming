1935bd865c470f7b66a279d908660e4f3c6cbcfb
potatological <te.zahra@gmail.com>
2023-04-16 21:15:04 -0700

    update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  Owned By Bwave/Bright
-*  Return: 0 is success
+*  reset_to_98 - function that takes a pointer and updates the value it points to
+*  @n: int to be checked
+*  
+*  Return: no return value
 */
 
 void reset_to_98(int *n)
