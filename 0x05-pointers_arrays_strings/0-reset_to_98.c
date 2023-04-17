f6d6d127b7c5036fb6bf18579d55479d036160c0
potatological <te.zahra@gmail.com>
2023-04-16 21:19:13 -0700

    update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
-*  reset_to_98 - function that takes a pointer and updates the value it points to
+*  reset_to_98 - function that takes a pointer
+*              - and updates the value it points to
 *  @n: int to be checked
-*  
+*
 *  Return: no return value
 */
 
