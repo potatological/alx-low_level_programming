ea927ae5ad23d5d7e41bd80dbe7c425956fc329b
potatological <te.zahra@gmail.com>
2023-04-16 22:05:31 -0700

    update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,14 @@
  */
 void puts2(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	  if (i % 2 == 0)
-	    _putchar(str[i]);
-	_putchar('\n');
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+}
+ _putchar('\n');
 }
